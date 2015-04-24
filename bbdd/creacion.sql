963b902059272e5f0b922a0d699b7c4dbb65755d
Sergio <sergio.reinoso@a20pc11.jda.ins>
2015-04-24 20:10:30 +0200

    First commit

---
+++ bbdd/creacion.sql
@@ -0,0 +1,22 @@
+create database brain;
+use brain;
+
+create table PREGUNTA (
+    id int auto_increment,
+    enunciat varchar(200),
+    resposta1 varchar(50),
+    resposta2 varchar(50),
+    resposta3 varchar(50),
+    resposta4 varchar(50),
+    respostacorrecta varchar(50),
+    primary key(id)
+) 
+ 
+create table JUGADOR (
+    id int auto_increment,
+    nickname varchar(200),
+    pwd varchar(200),
+    experiencia_total int,
+    partides_jugades int,
+    primary key(id)
+) 
