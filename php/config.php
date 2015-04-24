8c2694cbce8285155458dcce4f2289ce772307e4
Sergio <sergio.reinoso@a20pc11.jda.ins>
2015-04-24 20:11:53 +0200

    Add php files

---
+++ php/config.php
@@ -0,0 +1,9 @@
+<?php 
+/*
+**Almacena en variables los parametros de conexión a la base de datos
+**/
+	$dbHost = 'localhost';//'127.0.0.1'; - Host[:puerto] 
+	$dbUser = 'root'; // - Usuario 
+	$dbPass = '';//'austria'; //- Contraseña 
+	$dbName = 'uf3_m6_shoponline'; 
+?>
