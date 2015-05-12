5a34623106a9b0d0a3ee2aa8e53b153228ee916b
root <root@debian>
2015-05-13 00:52:31 +0200

    Corrección acentos bbdd

--- bbdd/creacion.sql
+++ bbdd/creacion.sql
@@ -22,15 +22,15 @@ create table JUGADOR (
 ) ;
 
 INSERT INTO `PREGUNTA` (`id`, `enunciat`, `resposta1`, `resposta2`, `resposta3`, `resposta4`, `respostacorrecta`) VALUES
-(1, '¿Cómo se llaman las crías de la mula?¿Cuantas crías tiene una mula?', '2', '4', '0', '1', '2'),
-(2, '¿Qué instrumento musical tiene nombre y forma geométricos?', 'Círculo', 'Cuadrado', 'Rombo', 'Triángulo', '3'),
-(3, '¿Con qué nombre firmaba Pablo Picasso sus pinturas?', 'Pablo', 'Picasso', 'Pablo Picasso', 'Pabicasso', '1'),
-(4, '¿Cuál es el único mamífero con cuatro rodillas?', 'La zebra', 'El elefante', 'El mono', 'El gorila', '1'),
-(5, '¿Con qué dos colores suele tener problemas un daltónico?', 'Rojo y verde', 'Amarillo y verde', 'Azul y rojo', 'Verde y Violeta', '0'),
-(6, '¿En qué deporte se usa tiza?', 'Ciclismo', 'Aerobic', 'Billar', 'Tiro al arco', '2'),
-(7, '¿Cuántas pirámides hay en Egipto?', '3', '10000', '1000', '10', '1'),
-(8, '¿Qué pie puso primero Neil Amstrong sobre la Luna?', 'No puso ningún pie, puso primero la bandera', 'Los dos a la vez', 'Derecho', 'Izquierdo', '3'),
-(9, '¿Cómo se transmiten más rápidamente las ondas sonoras?', 'Por Tierra', 'Por Aire', 'Por Agua', 'Por Fuego', '2'),
-(10, '¿Qué parte del cuerpo tiene los huesos más pequeños?', 'Mano', 'Pie', 'Oído', 'Pierna', '2');
+(1, 'Como se llaman las crias de la mulaCuantas crias tiene una mula', '2', '4', '0', '1', '2'),
+(2, 'Que instrumento musical tiene nombre y forma geometricos', 'Circulo', 'Cuadrado', 'Rombo', 'Triangulo', '3'),
+(3, 'Con que nombre firmaba Pablo Picasso sus pinturas', 'Pablo', 'Picasso', 'Pablo Picasso', 'Pabicasso', '1'),
+(4, 'Cual es el unico mamifero con cuatro rodillas', 'La zebra', 'El elefante', 'El mono', 'El gorila', '1'),
+(5, 'Con que dos colores suele tener problemas un daltonico', 'Rojo y verde', 'Amarillo y verde', 'Azul y rojo', 'Verde y Violeta', '0'),
+(6, 'En que deporte se usa tiza', 'Ciclismo', 'Aerobic', 'Billar', 'Tiro al arco', '2'),
+(7, 'Cuantas piramides hay en Egipto', '3', '10000', '1000', '10', '1'),
+(8, 'Que pie puso primero Neil Amstrong sobre la Luna', 'No puso ningun pie, puso primero la bandera', 'Los dos a la vez', 'Derecho', 'Izquierdo', '3'),
+(9, 'Como se transmiten mas rapidamente las ondas sonoras', 'Por Tierra', 'Por Aire', 'Por Agua', 'Por Fuego', '2'),
+(10, 'Que parte del cuerpo tiene los huesos mas pequenos', 'Mano', 'Pie', 'Oido', 'Pierna', '2');
  
  INSERT INTO `brain`.`JUGADOR` (`id`, `nickname`, `pwd`, `experiencia_total`, `partides_jugades`) VALUES (NULL, 'aaa', PASSWORD('aaa'), '0', '0'), (NULL, 'bbb', PASSWORD('bbb'), '0', '0');
